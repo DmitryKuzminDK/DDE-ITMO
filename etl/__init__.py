9e8140c5cef7bdfc9d35e6ccdcfff465706886b4
DmitryKuzminDK <nukakvass@gmail.com>
2026-03-10 08:36:46 +0300

    Update __init__.py

--- etl/__init__.py
+++ etl/__init__.py
@@ -1,10 +1,1 @@
-# Пакет ETL модулей
-from .extract import DataExtractor, extract_from_kaggle, extract_from_local, extract_from_env
-from .transform import DataTransformer, transform_data, quick_clean
-from .load import DataLoader, load_data, load_from_processed
-
-__all__ = [
-    'DataExtractor', 'extract_from_kaggle', 'extract_from_local', 'extract_from_env',
-    'DataTransformer', 'transform_data', 'quick_clean',
-    'DataLoader', 'load_data', 'load_from_processed'
-]
+# Пустой файл
